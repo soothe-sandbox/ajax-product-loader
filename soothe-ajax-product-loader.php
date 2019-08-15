b4956ad320960cad3056fc01db8f4ec43e7acadb
soothe <soothe.box.github@yandex.ua>
2019-08-15 20:05:52 +0200

    Add main php file, with proper Wp Plugin Header

---
+++ soothe-ajax-product-loader.php
@@ -0,0 +1,15 @@
+<?
+/**
+ * Plugin Name:       Ajax Product Loader
+ * Plugin URI:        https://github.com/soothe-sandbox/ajax-product-loader
+ * Description:       Plugin for Wordpress Storefront theme, for ajax product loading.
+ * Version:           0.1
+ * Requires at least: 5.0
+ * Requires PHP:      7.0
+ * Author:            Soothe
+ * Author URI:        https://github.com/soothe-sandbox
+ * License:           MIT License
+ * License URI:       https://github.com/soothe-sandbox/ajax-product-loader/blob/master/LICENSE
+ * Text Domain:       soothe-ajax-product-loader
+ * Domain Path:       /languages
+ */
